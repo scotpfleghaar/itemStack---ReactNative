1c91180515c131cd2eff249c53b6317a2cc036dc
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-16 14:32:16 -0500

    Calling action creators to make them available to our class.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {CardSection} from "./common";
 import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 class ListItem extends Component {
     render() {
         const {titleStyle} = styles;
@@ -22,4 +24,5 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ListItem;
+
+export default connect(null, actions)(ListItem);
