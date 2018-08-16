c08ddf61cf74e0af0d65155bf9d9e3891ce56193
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-16 08:12:53 -0500

    Hooking up Redux with React Native

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './src/reducers';
 
 type Props = {};
 export default class App extends Component<Props> {
     render() {
         return (
-            <View>
-
-            </View>
+            <Provider store={createStore(reducers)}>
+                <View style={styles.container}>
+                    <Text>HELLO!</Text>
+                </View>
+            </Provider>
         );
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
