92c1e105a2cc2e599c6559814da0cca24ad1edea
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-08-16 14:09:45 -0500

    Creating the selection reducer, which will provide the ID of the selected Item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {CardSection} from "./common";
-
+import {connect} from 'react-redux';
 class ListItem extends Component {
     render() {
         const {titleStyle} = styles;
+
+
         return (
             <CardSection>
                 <Text style={titleStyle}>{this.props.library.item.title}</Text>
